85821d4ce2a433f61c866525b025b5be287141e2
Wellytnon.spagnol <wellyntonspagnol10@gmail.com>
2024-01-06 22:14:25 -0300

    Initial commit

---
+++ src/main/java/com/api/cashcard/CashCardApplication.java
@@ -0,0 +1,13 @@
+package com.api.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashCardApplication.class, args);
+    }
+
+}
