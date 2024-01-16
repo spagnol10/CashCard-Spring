c26ad093fed3106e497aeaced4fd7b383f585d43
Wellytnon.spagnol <wellyntonspagnol10@gmail.com>
2024-01-16 20:31:53 -0300

    New module

--- src/main/java/com/api/cashcard/controller/CashCardController.java
+++ src/main/java/com/api/cashcard/controller/CashCardController.java
@@ -5,6 +5,7 @@ import com.api.cashcard.repository.CashCardRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -39,7 +40,7 @@ public class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
     }
--- src/test/java/com/api/cashcard/controller/CashCardControllerTest.java
+++ src/test/java/com/api/cashcard/controller/CashCardControllerTest.java
@@ -105,5 +105,18 @@ class CashCardControllerTest {
         assertThat(page.size()).isEqualTo(1);
     }
 
+    @Test
+    void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        JSONArray page = documentContext.read("$[*]");
+        assertThat(page.size()).isEqualTo(3);
+
+        JSONArray amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
+    }
+
 
 }
