986839234a3d0dec200e026fa24d4984e1fcb1c3
wellynton.spagnol <wellynton.spagnol@aliare.co>
2024-01-16 10:34:16 -0300

    add testes para listas

--- src/test/java/com/api/cashcard/dto/CashCardJsonTest.java
+++ src/test/java/com/api/cashcard/dto/CashCardJsonTest.java
@@ -1,6 +1,7 @@
 package com.api.cashcard.dto;
 
-import com.api.cashcard.dto.CashCard;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -16,6 +17,39 @@ class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 1.00),
+                new CashCard(101L, 150.00));
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/test/java/com/api/cashcard/json/list.json");
+
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson(fileInputStream);
+
+    }
+
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expected = """
+                [
+                    { "id": 99, "amount": 123.45 },
+                    { "id": 100, "amount": 100.00 },
+                    { "id": 101, "amount": 150.00 }
+                ]
+                """;
+        assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
